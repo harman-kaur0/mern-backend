6682440df890779ef4efa62437a19afa631ae795
Harmandeep Kaur <harmandeepk628@gmail.com>
2021-09-01 22:54:38 -0700

    created user model and controller and routes file

--- index.js
+++ index.js
@@ -2,3 +2,24 @@ import express from "express"
 import bodyParser from "body-parser"
 import mongoose from "mongoose"
 import cors from "cors"
+import dotenv from "dotenv"
+import userRoutes from "./routes/users.js"
+
+const app = express();
+dotenv.config();
+
+app.use(bodyParser.json( {limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+app.use(express.json());
+
+app.use('/user', userRoutes);
+
+const URI = process.env.ATLAS_URI;
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+// mongoose.set('useFindAndModify', false);
