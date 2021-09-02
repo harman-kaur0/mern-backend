6aab5ba1aa60cf7d37a0695aa50fbf054094e1bf
Harmandeep Kaur <harmandeepk628@gmail.com>
2021-09-02 15:44:55 -0700

    finished implementing the sign in and signup for user

--- controllers/user.js
+++ controllers/user.js
@@ -14,11 +14,32 @@ export const signin = async (req, res) => {
         if(!isPasswordCorrect) return res.status(400).json({ message: "Invalid credentials" })
 
         const token = jwt.sign({ email: existingUser.email, id: existingUser._id }, 'test', { expiresIn: "1h" })
-    } catch (error) {
 
+        res.status(200).json({ result: existingUser, token });
+    } catch (error) {
+        res.status(500).json({ message: 'Something went wrong. '});
     }
 }
 
 export const signup = async (req, res) => {
-    
+    const { email, password, confirmPassword, firstName, lastName } = req.body;
+
+    console.log(req.body)
+
+    try {
+        const existingUser = await User.findOne({ email });
+        if(existingUser) return res.status(400).json({ message: "User already exists." })
+
+        if(password != confirmPassword) return res.status(400).json({ message: "Password doesn't match." })
+
+        const hashedPassword = await bcrypt.hash(password, 12);
+
+        const result = await User.create({ email, password: hashedPassword, name: `${firstName} ${lastName}`});
+
+        const token = jwt.sign({ email: result.email, id: result._id }, 'test', { expiresIn: "1h" })
+
+        res.status(200).json({ result, token });
+    } catch (error){
+        res.status(500).json({ message: 'Something went wrong. '});
+    }
 }
--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use(express.json());
 
-app.use('/user', userRoutes);
+app.use('/users', userRoutes);
 
 const URI = process.env.ATLAS_URI;
 const PORT = process.env.PORT || 5000;
